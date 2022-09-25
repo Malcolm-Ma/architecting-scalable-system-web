bd21b3c0feef6317b6e76fbb6fa6893b88e2e8b8
HUANG YUZHE <Huang_Yuzhe_from.tp@simtech.a-star.edu.sg>
2022-09-25 18:09:44 +0800

    feature(dev): Integrate Keycloak Service with ReactJS frontend

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import KeycloakService from './services/KeycloakService';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component<any, any> {
+
+  constructor(props: any) {
+    super(props);
+  }
+
+  render() {
+    console.log(KeycloakService.getToken());
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {KeycloakService.isLoggedIn()
+            ? <button className="btn btn-lg btn-warning" onClick={() => KeycloakService.doLogout()}>Logout</button>
+            : <button className="btn btn-lg btn-warning" onClick={() => KeycloakService.doLogin()}>Login</button>
+          }
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
