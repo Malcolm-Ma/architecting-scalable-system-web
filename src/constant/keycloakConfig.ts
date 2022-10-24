9f89002e041e62d88971b1492542a86354d9e001
Trekker-one <huangyuzhe2019@gmail.com>
2022-10-24 22:04:29 +0800

    feature: split DEV and PROD environment

--- src/constant/keycloakConfig.ts
+++ src/constant/keycloakConfig.ts
@@ -1,3 +1,3 @@
 export const KC_REALM_NAME = "demo";
 export const KC_URL = "https://authsvc-acs.com/";
-export const KC_CLIENT_ID = "DEMO_CLIENT";
+export const KC_CLIENT_ID = process.env.NODE_ENV === 'development' ? "DEMO_CLIENT" : "ELEARN_CLIENT";
--- src/constant/network.ts
+++ src/constant/network.ts
@@ -3,6 +3,6 @@
  * @author Mingze Ma
  */
 
-export const SERVICE_BASE_URL = 'http://localhost:8090/api';
+export const SERVICE_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8090/api' : 'https://elearnteam3.com/api';
 
 export const KEYCLOAK_BASE_URL = 'https://authsvc-acs.com';
