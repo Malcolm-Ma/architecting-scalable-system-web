7935b14194e7baffdc37c411ac67dd69c701cc1e
Mingze Ma <mingzema.1998@foxmail.com>
2022-10-24 21:55:18 +0800

    feature(commodity): cart list

--- src/actions/order.ts
+++ src/actions/order.ts
@@ -6,3 +6,8 @@ import api from "src/api";
 import apiConfig from "src/api/apiConfig";
 
 export const addModuleToCart = (params: any) => api.post(apiConfig.order.cart.create, params);
+
+export const getCartList = (params: any) => api.get(apiConfig.order.cart.display, params);
+
+export const removeCommodityFromCart = (params: any) => api.post(apiConfig.order.cart.remove, params);
+
