ee7c76b55f838c60450cad4c7e3fd7e77642bdd8
Mingze Ma <mingzema.1998@foxmail.com>
2022-10-26 03:55:33 +0800

    feature: commodity img

--- src/module/commodity/CommodityBrief.tsx
+++ src/module/commodity/CommodityBrief.tsx
@@ -35,7 +35,8 @@ const CommodityBrief: React.FC<CommodityBriefProps> = (props) => {
         user_id: _.get(user, 'userInfo.user_id'),
         commodity_id: _.get(data, 'commodity_id'),
       });
-      message.success(`Add ${_.get(data, 'commodity_name')} to cart successfully.`)
+      message.success(`Add ${_.get(data, 'commodity_name')} to cart successfully.`);
+      setTimeout(() => window.location.reload(), 1000);
     } catch (e) {
       console.error(e)
     }
@@ -87,10 +88,38 @@ const CommodityBrief: React.FC<CommodityBriefProps> = (props) => {
               discount={_.get(data, 'commodity_discount', 1)}
               priceProps={{variant: 'h3', color: '#fff'}}
               discountProps={{variant: 'h4'}}
-              boxStyle={{display: 'flex', alignItems: 'center', py: 0}}
+              boxStyle={{display: 'flex', alignItems: 'center', pb: 0, pt: 2}}
             />
+            <Button
+              size="large"
+              variant="contained"
+              sx={{
+                mt: {xs: 4, sm: 6},
+                bgcolor: 'rgb(167, 131, 55)',
+                '&:disabled': {
+                  color: '#fff',
+                  bgcolor: 'rgba(167, 131, 55, .8)',
+                },
+              }}
+              onClick={handleAddCartClick}
+              disabled={inCart}
+            >
+              {!inCart ? 'Add to Cart' : 'Already In Cart'}
+            </Button>
           </Grid>
           <Grid item xs={12} sm={6} sx={{px: {xs: 0, sm: 6}}}>
+            <Box
+              sx={{
+                backgroundImage: `url(${_.get(data, 'commodity_cover')})`,
+                backgroundSize: 'contain',
+                backgroundPosition: 'left',
+                backgroundRepeat: 'no-repeat',
+                height: theme.spacing(20),
+                margin: 'auto',
+                mb: 2,
+                mt: {xs: 2, sm: 0}
+              }}
+            ></Box>
             <Typography variant="h4" color="primary.contrastText" sx={{pb: 2, pt: {xs: 2, sm: 0}}}>
               Offered by
             </Typography>
@@ -103,23 +132,6 @@ const CommodityBrief: React.FC<CommodityBriefProps> = (props) => {
             </Typography>
           </Grid>
         </Grid>
-        <Button
-          size="large"
-          variant="contained"
-          sx={{
-            ml: {xs: 0, sm: 2},
-            mt: 4,
-            bgcolor: 'rgb(167, 131, 55)',
-            '&:disabled': {
-              color: '#fff',
-              bgcolor: 'rgba(167, 131, 55, .8)',
-            },
-          }}
-          onClick={handleAddCartClick}
-          disabled={inCart}
-        >
-          {!inCart ? 'Add to Cart': 'Already In Cart'}
-        </Button>
       </Container>
     </Box>
   );
