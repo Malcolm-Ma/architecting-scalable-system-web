ada0d54ce486e5a52fbac350398822fd03714776
Mingze Ma <mingzema.1998@foxmail.com>
2022-10-25 21:22:37 +0800

    feature(search): search api config

--- src/constant/network.ts
+++ src/constant/network.ts
@@ -6,3 +6,5 @@
 export const SERVICE_BASE_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8090/api' : 'https://elearnteam3.com/api';
 
 export const KEYCLOAK_BASE_URL = 'https://authsvc-acs.com';
+
+export const SERVICE_SEARCH_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:8091' : 'http://http://35.240.236.234/';
